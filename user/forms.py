93bd48bcd93db96a9ad3f3dd97fc6e47a5139b1b
0422-shiv <shiveshbhardwaj149@gmail.com>
2022-03-22 22:47:52 +0530

    user module

--- user/forms.py
+++ user/forms.py
@@ -1,21 +1,3 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django.forms import ModelForm
-from user.models import RagisterUser
 
-
-
-class UserForm(UserCreationForm):
-                
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-
-class RagisterForm(ModelForm):
-
-    class Meta:
-        model = RagisterUser
-        fields = ['mobile_number', 'roll', 'address', 'term_condition']
                 
 
