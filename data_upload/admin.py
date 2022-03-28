04968d517d9b7eaa5bb5e8172dc49834036fc244
0422-shiv <shiveshbhardwaj149@gmail.com>
2022-03-29 00:25:10 +0530

    paper upload ,login,register

--- data_upload/admin.py
+++ data_upload/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Semester, Course , ExamPaper 
 # Register your models here.
+
+admin.site.register(Semester)
+admin.site.register(Course)
+admin.site.register(ExamPaper)
--- data_upload/views.py
+++ data_upload/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.views import generic
+from data_upload.models import Course, Semester
+from data_upload.forms import DataUploadForm
+from django.contrib import messages
 
 # Create your views here.
 
-
 class DataUploadView(generic.TemplateView):
-    template_name = "data-upload.html"  
+    template_name = "data-upload.html" 
+
+    def get(self, request):
+        courses = Course.objects.all()
+        semester = Semester.objects.all()
+        return render(request,  "data-upload.html",{'courses':courses, 'semester':semester}) 
+
+    def post(self,request):
+        form = DataUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()  
+            messages.info(request, 'Your Data Successfully Upload.')
+            return render(request, "data-upload.html")
+        else:
+            messages.info(request, 'Your Data has not Upload ! You fill the right Data')
+            form = DataUploadForm()
+        return render(request, "data-upload.html")  
